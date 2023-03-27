ceee176bfb73b9ee5cba94047297c860daa50a38
UihwanLee <luh5063014@naver.com>
2023-03-27 23:49:36 +0900

    Upload Project
    Drawing 애니메이션 추가 && Player_Return 애니메이션 추가

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneManager : MonoBehaviour
 {
@@ -11,11 +12,39 @@ public class SceneManager : MonoBehaviour
     void Start()
     {
         uiManager = GameObject.Find("UIManager").GetComponent<UIManager>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public void ThinkPicture(Text _textUI, string _text, GameObject _selectUI)
     {
-        
+        StartCoroutine(ThinkPictureCoroutine(_textUI, _text, _selectUI));
+    }
+
+    IEnumerator ThinkPictureCoroutine(Text _textUI, string _text, GameObject _selectUI)
+    {
+        yield return new WaitForSeconds(1f);
+        uiManager.SetETCUI(true, false);
+        yield return new WaitForSeconds(0.5f);
+        uiManager.SetETCUI(true, true);
+        yield return new WaitForSeconds(0.6f);
+        uiManager.SetSelectCanvasUI(true);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(Typing(_textUI, _text));
+        yield return new WaitForSeconds(1f);
+        _selectUI.SetActive(true);
+    }
+
+    // 자막 타이핑 이펙트
+    IEnumerator Typing(Text _textUI, string _text)
+    {
+        _textUI.text = "";
+        for (int i = 0; i <= _text.Length; i++)
+        {
+            _textUI.text = _text.Substring(0, i);
+            //textBox.GetComponent<Text>().text = text.Substring(0, i);
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,6 +8,11 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Pitcure[] pictures;
 
+    // 단계
+    // 1) 밑그림
+    // 2) 색칠
+    private int stage;
+
     // Select Canvas UI
     [SerializeField]
     private Text question;
@@ -17,39 +22,59 @@ public class UIManager : MonoBehaviour
     private GameObject selectPictureUI;
     [SerializeField]
     private GameObject selectColorUI;
+    [SerializeField]
+    private GameObject[] etcs;
 
+    // Check Canvas
+    [SerializeField]
+    private GameObject checkCanvasUI;
+
+    [SerializeField]
+    private Animator animatorDraw;
     // 캔버스에 표시할 Sprite
     [SerializeField]
-    private GameObject drawCanvas;
+    private GameObject drawCanvas_Default;
+    [SerializeField]
+    private GameObject drawCanvas_Color;
 
     [SerializeField]
     private Material M_picture;
 
     private Pitcure curPicture;
 
+    private SceneManager sceneManager;
+
+    [SerializeField]
+    private Animator playerAnim;
+
     private void Start()
     {
-        question.text = "What's Draw?";
+        stage = 0;
+        question.text = "";
 
         curPicture = null;
         selectColorUI.SetActive(false);
-        //selectPictureUI.SetActive(false);
-        //selectCanvasUI.SetActive(false);
-        drawCanvas.GetComponent<SpriteRenderer>().sprite = null;
+        InitSelectPictureUI();
+        checkCanvasUI.SetActive(false);
+
+        etcs[0].SetActive(false);
+        etcs[1].SetActive(false);
+        drawCanvas_Default.GetComponent<SpriteRenderer>().sprite = null;
+        drawCanvas_Color.GetComponent<SpriteRenderer>().sprite = null;
+
+        sceneManager = GameObject.Find("SceneManager").GetComponent<SceneManager>();
+        sceneManager.ThinkPicture(question, "What's Draw?", selectPictureUI);
     }
 
-    // 밑그림 캔버스 선택 시
-    public void SelectPicture(GameObject _pictureImage)
+    private void InitSelectPictureUI()
     {
-        // 그림 그리는 애니메이션 작동 후 그림 그리기
-        curPicture = _pictureImage.GetComponent<SelectPicture>().GetCurPicture();
-        drawCanvas.GetComponent<SpriteRenderer>().sprite = _pictureImage.transform.GetChild(0).GetComponent<Image>().sprite;
-
-        question.text = "What's Color";
+        selectCanvasUI.SetActive(true);
+        selectPictureUI.SetActive(true);
+        selectPictureUI.transform.GetChild(0).transform.GetChild(1).GetComponent<Text>().text = "";
+        selectPictureUI.transform.GetChild(1).transform.GetChild(1).GetComponent<Text>().text = "";
+        selectPictureUI.transform.GetChild(2).transform.GetChild(1).GetComponent<Text>().text = "";
         selectPictureUI.SetActive(false);
         selectCanvasUI.SetActive(false);
-
-        StartCoroutine(ShowSelectPictureUI());
     }
 
     public void SetSelectCanvasUI(bool _active)
@@ -57,26 +82,82 @@ public class UIManager : MonoBehaviour
         selectCanvasUI.SetActive(_active);
     }
 
-    IEnumerator ShowSelectPictureUI()
+    // 밑그림 캔버스 선택 시
+    public void SelectPicture(GameObject _pictureImage)
     {
-        yield return new WaitForSeconds(5f);
+        // 그림 그리는 애니메이션 작동 후 그림 그리기
+        curPicture = _pictureImage.GetComponent<SelectPicture>().GetCurPicture();
+        drawCanvas_Default.GetComponent<SpriteRenderer>().sprite = _pictureImage.transform.GetChild(0).GetComponent<Image>().sprite;
+        animatorDraw.SetTrigger("Draw_Default");
 
+        question.text = "";
+        InitSelectPictureUI();
 
-        selectCanvasUI.SetActive(true);
-        selectColorUI.SetActive(true);
+        // 다 그린 후 보여주기
+        etcs[0].SetActive(false);
+        etcs[1].SetActive(false);
+
+        checkCanvasUI.SetActive(true);
+
+        //sceneManager.ThinkPicture(question, "What's Color?", selectColorUI);
+    }
+
+    public void SetSelectPictureUI(bool _active)
+    {
+        selectPictureUI.SetActive(_active);
     }
 
     public void SelectColors(Image _colors)
     {
+        question.text = "";
         selectColorUI.SetActive(false);
         selectCanvasUI.SetActive(false);
+        etcs[0].SetActive(false);
+        etcs[1].SetActive(false);
 
-        if(curPicture != null)
+        if (curPicture != null)
         {
-            drawCanvas.GetComponent<SpriteRenderer>().sprite = curPicture.GetPictureColor();
-            drawCanvas.GetComponent<SpriteRenderer>().material = M_picture;
-            drawCanvas.GetComponent<SpriteRenderer>().material.SetColor("_PictureColor1", Color.green);
-            drawCanvas.GetComponent<SpriteRenderer>().material.SetColor("_PictureColor2", _colors.color);
+            animatorDraw.SetTrigger("Draw_Color");
+            drawCanvas_Color.GetComponent<SpriteRenderer>().sprite = curPicture.GetPictureColor();
+            drawCanvas_Color.GetComponent<SpriteRenderer>().material = M_picture;
+            drawCanvas_Color.GetComponent<SpriteRenderer>().material.SetColor("_PictureColor1", Color.green);
+            drawCanvas_Color.GetComponent<SpriteRenderer>().material.SetColor("_PictureColor2", _colors.color);
+
+            checkCanvasUI.SetActive(true);
+        }
+    }
+
+    public void SetSelectColorUI(bool _active)
+    {
+        selectColorUI.SetActive(_active);
+    }
+
+    public void SetETCUI(bool _active1, bool _active2)
+    {
+        etcs[0].SetActive(_active1);
+        etcs[1].SetActive(_active2);
+    }
+
+    public void ReDraw()
+    {
+        checkCanvasUI.SetActive(false);
+        if(stage == 0) sceneManager.ThinkPicture(question, "What's Draw?", selectPictureUI);
+        else if (stage == 1) sceneManager.ThinkPicture(question, "What's Color?", selectColorUI);
+    }
+
+
+    public void Done()
+    {
+        checkCanvasUI.SetActive(false);
+        if (stage == 0)
+        {
+            sceneManager.ThinkPicture(question, "What's Color?", selectColorUI);
+            stage++;
+        }
+        else if (stage == 1)
+        {
+            // SceneManager에서 엔딩 보기
+            playerAnim.SetTrigger("Done");
         }
     }
 }
